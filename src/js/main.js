836d2530e7f7cb013c474bd5c0a3836a35afd2e3
Nadezhda Rachkova <nadezda.rachkova@mail.ru>
2023-10-27 13:15:16 +0300

    Коммит

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,34 @@
-import isMobile from './components/is-mobile';
-import sliders from './components/sliders';
-import gumburger from './components/gumburger';
-import popups from './components/js-popup';
-import validate from './components/validate';
-import accordion from './components/js-accordion';
-import tables from './components/tables';
-import mainMenu from './components/js-main-menu';
-import tabsComp from './components/tabs';
+import isMobile from './components/is-mobile'
+import sliders from './components/sliders'
+import gumburger from './components/gumburger'
+import popups from './components/js-popup'
+import validate from './components/validate'
+import accordion from './components/js-accordion'
+import tables from './components/tables'
+import mainMenu from './components/js-main-menu'
+import tabsComp from './components/tabs'
 
-import mask from './components/js-mask';
-import svg4everybody from './components/svg4everybody';
-import svgStore from './components/svg-store';
+import mask from './components/js-mask'
+import svg4everybody from './components/svg4everybody'
+import svgStore from './components/svg-store'
 
-$(function() {
-  let htmlEl = document.documentElement;
+$(function () {
+  let htmlEl = document.documentElement
   if (isMobile()) {
-    htmlEl.classList.add('is-mobile');
+    htmlEl.classList.add('is-mobile')
   } else {
-    htmlEl.classList.add('is-not-mobile');
+    htmlEl.classList.add('is-not-mobile')
   }
 
-  sliders();
-  gumburger();
-  popups();
-  validate();
-  accordion();
-  tables();
-  mainMenu();
-  tabsComp();
-  mask();
-  svg4everybody();
-  svgStore();
-});
+  sliders({a: '1', b: '2'})
+  gumburger()
+  popups()
+  validate()
+  accordion()
+  tables()
+  mainMenu()
+  tabsComp()
+  mask()
+  svg4everybody()
+  svgStore()
+})
